ca67f3cab327dc7d915c4e570132a6d843b95940
PythonKat <105568004+MMMystical@users.noreply.github.com>
2024-03-03 20:06:36 +0200

    Chat Bypasser v1.91 🍀

--- Chat Bypasser/src.lua
+++ Chat Bypasser/src.lua
@@ -23,7 +23,7 @@ local gui = Library:create{
 }
 
 gui:set_status("Status | Bypassing chat... LoL")
-gui:set_scriptdisplay("Chat Bypasser v1.91 🍀" .. libVersion)
+gui:set_scriptdisplay("Chat Bypasser v1.91 🍀")
 
 gui:notification{
 	Title = "Chat Bypasser v1.91 🍀",
