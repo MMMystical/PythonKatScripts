bfd9be39a36f3c4d399e98925955fe295fec356d
Mystic <105568004+MMMystical@users.noreply.github.com>
2025-04-26 02:13:26 +0300

    main script

--- Chat Bypasser/chatbypasser.lua
+++ Chat Bypasser/chatbypasser.lua
@@ -1,2 +1,2 @@
 --copy paste this loadstring to load the script
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/MMMystical/PythonKatScripts/main/Chat%20Bypasser/src.lua"),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/MMMystical/PythonKatScripts/main/Chat%20Bypasser/src.lua")))()
