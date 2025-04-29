fa229e0a75250defebfef4ec675769d604087cec
Mystic <105568004+MMMystical@users.noreply.github.com>
2025-04-29 16:31:43 +0300

    Update chainaimbot.lua

--- CHAIN Abuser/Essentials/chainaimbot.lua
+++ CHAIN Abuser/Essentials/chainaimbot.lua
@@ -1,60 +1,61 @@
-local hijklmnopq = {}
-hijklmnopq.__index = hijklmnopq
-local DEFGHvwxyz = game:GetService(string.char(68,69,70,71,72,118,119,120,121,122))
-local bcdefghijz = game:GetService(string.char(98,99,100,101,102,103,104,105,106,122))
-local STUVWXmnop = game:GetService(string.char(83,84,85,86,87,88,109,110,111,112))
-local stuvwxyzAB = DEFGHvwxyz:WaitForChild(string.char(77,105,115,99))
-local cdefghijkl = stuvwxyzAB:WaitForChild(string.char(65,73))
-local HIJKLMNOPQ = DEFGHvwxyz.CurrentCamera
-local fghijklmno = (not true)
-local mnopqrstuv
+local abcdefghij = {}
+abcdefghij.__index = abcdefghij
+local Workspace = game:GetService(string.char(87,111,114,107,115,112,97,99,101))
+local RunService = game:GetService(string.char(82,117,110,83,101,114,118,105,99,101))
+local UserInputService = game:GetService(string.char(85,115,101,114,73,110,112,117,116,83,101,114,118,105,99,101))
+local cdefghijkl = Workspace:WaitForChild(string.char(77,105,115,99))
+local yzABCDE = cdefghijkl:WaitForChild(string.char(65,73))
+local Camera = Workspace.CurrentCamera
+local lmnopqrstu = (not true)
+local FGHIJKLMNO
+local stuvwxyzAB
 local defghijklm
-local IJKLMNOPQR
-local function fghijklmno(GHIJKLMNOP)
- local ABCDEFGHIJ = CFrame.new(HIJKLMNOPQ.CFrame.Position, GHIJKLMNOP.Position)
- HIJKLMNOPQ.CFrame = ABCDEFGHIJ
+local function ghijklmnop(defghijklm)
+ local defghijklm = CFrame.new(Camera.CFrame.Position, defghijklm.Position)
+ Camera.CFrame = defghijklm
 end
-local function cdefghiefg()
- for _, child in ipairs(cdefghijkl:GetChildren()) do
+local function uvwxyzABCD()
+ for _, child in ipairs(yzABCDE:GetChildren()) do
  if child:FindFirstChild(string.char(72,117,109,97,110,111,105,100,82,111,111,116,80,97,114,116)) then
  return child
  end
  end
 end
-function hijklmnopq:enable()
+function abcdefghij:enable()
  self.enabled = (not (not true))
- mnopqrstuv = bcdefghijz.RenderStepped:Connect(function()
+ FGHIJKLMNO = RunService.RenderStepped:Connect(function()
  if not self.enabled then return end
- if not fghijklmno then return end
- local MNOPQRSrst = cdefghiefg()
- if MNOPQRSrst then
- fghijklmno(MNOPQRSrst:GetPivot())
+ if not lmnopqrstu then return end
+ local QRSTUVWXjk = uvwxyzABCD()
+ if QRSTUVWXjk then
+ ghijklmnop(QRSTUVWXjk:GetPivot())
  end
  end)
- defghijklm = STUVWXmnop.InputBegan:Connect(function(abcdefghij)
- if abcdefghij.UserInputType == Enum.UserInputType.MouseButton2 then
- fghijklmno = (not (not true))
+ stuvwxyzAB = UserInputService.InputBegan:Connect(function(tuvwxyzABC)
+ if tuvwxyzABC.UserInputType == Enum.UserInputType.MouseButton2 then
+ lmnopqrstu = (not (not true))
  end
  end)
- IJKLMNOPQR = STUVWXmnop.InputEnded:Connect(function(abcdefghij)
- if abcdefghij.UserInputType == Enum.UserInputType.MouseButton2 then
- fghijklmno = (not true)
+ defghijklm = UserInputService.InputEnded:Connect(function(tuvwxyzABC)
+ if tuvwxyzABC.UserInputType == Enum.UserInputType.MouseButton2 then
+ lmnopqrstu = (not true)
  end
  end)
 end
-function hijklmnopq:disable()
+function abcdefghij:disable()
  self.enabled = (not true)
- if mnopqrstuv then
- mnopqrstuv:Disconnect()
- mnopqrstuv = nil
+ if FGHIJKLMNO then
+ FGHIJKLMNO:Disconnect()
+ FGHIJKLMNO = nil
+ end
+ if stuvwxyzAB then
+ stuvwxyzAB:Disconnect()
+ stuvwxyzAB = nil
  end
  if defghijklm then
  defghijklm:Disconnect()
  defghijklm = nil
  end
- if IJKLMNOPQR then
- IJKLMNOPQR:Disconnect()
- IJKLMNOPQR = nil
- end
 end
-return setmetatable({}, hijklmnopq)
+return setmetatable({}, abcdefghij)
+print('Obfuscated with PythonKat Obfuscator v1')
