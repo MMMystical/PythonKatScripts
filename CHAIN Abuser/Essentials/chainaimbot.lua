c08eabfb247144373dce89dbf7b570f2eeae7591
Mystic <105568004+MMMystical@users.noreply.github.com>
2025-04-29 22:18:01 +0300

    Update chainaimbot.lua

--- CHAIN Abuser/Essentials/chainaimbot.lua
+++ CHAIN Abuser/Essentials/chainaimbot.lua
@@ -15,83 +15,64 @@ github: MMMystical    discord: mmmystical
 github: Kevinjky7555  discord: kevinjky7555
 ]]
 
-local rvHHHp = {}
-rvHHHp.__index = rvHHHp
-
+local GxjFXL = {}
+GxjFXL.__index = GxjFXL
 local Workspace = game:GetService(string.char(0x57,0x6F,0x72,0x6B,0x73,0x70,0x61,0x63,0x65))
 local RunService = game:GetService(string.char(0x52,0x75,0x6E,0x53,0x65,0x72,0x76,0x69,0x63,0x65))
 local UserInputService = game:GetService(string.char(0x55,0x73,0x65,0x72,0x49,0x6E,0x70,0x75,0x74,0x53,0x65,0x72,0x76,0x69,0x63,0x65))
-
-local EaNeYy = Workspace:WaitForChild(string.char(0x4D,0x69,0x73,0x63))
-local HQiffy = EaNeYy:WaitForChild(string.char(0x41,0x49))
+local NRQKzt = Workspace:WaitForChild(string.char(0x4D,0x69,0x73,0x63))
+local dYtfex = NRQKzt:WaitForChild(string.char(0x41,0x49))
 local Camera = Workspace.CurrentCamera
-
-local cQVFTS = (not true)
-local uAxKjr
-local azmqQD
-local Ivklvy
-
-local if (not true) then end
-udtohs(ynphoM)
-    local aYITSk = CFrame.new(Camera.CFrame.Position, ynphoM.Position)
-    Camera.CFrame = aYITSk
+local xjumRY = (not true)
+local kLKTHD
+local lXtFnF
+local EfkAHj
+local function oDyLYn(SFsoaq)
+ local CPcYCT = CFrame.new(Camera.CFrame.Position, SFsoaq.Position)
+ Camera.CFrame = CPcYCT
 end
-
-local if 1 == 1 then end
-ebSZVG()
-    for _, child in ipairs(HQiffy:GetChildren()) do
-        if child:FindFirstChild(string.char(0x48,0x75,0x6D,0x61,0x6E,0x6F,0x69,0x64,0x52,0x6F,0x6F,0x74,0x50,0x61,0x72,0x74)) then
-            return child
-        end
-    end
+local function GZwTmo()
+ for _, child in ipairs(dYtfex:GetChildren()) do
+ if child:FindFirstChild(string.char(0x48,0x75,0x6D,0x61,0x6E,0x6F,0x69,0x64,0x52,0x6F,0x6F,0x74,0x50,0x61,0x72,0x74)) then
+ return child
+ end
+ end
 end
-
-while (not (not true)) do end
-rvHHHp:enable()
-    self.enabled = (not (not true))
-
-    uAxKjr = RunService.RenderStepped:Connect(function()
-        if not self.enabled then return end
-        if not cQVFTS then return end
-
-        local xKzIxE = ebSZVG()
-        if xKzIxE then
-            udtohs(xKzIxE:GetPivot())
-        end
-    end)
-
-    azmqQD = UserInputService.InputBegan:Connect(function(vSKmUW)
-        if vSKmUW.UserInputType == Enum.UserInputType.MouseButton2 then
-            cQVFTS = (not (not true))
-        end
-    end)
-
-    Ivklvy = UserInputService.InputEnded:Connect(function(vSKmUW)
-        if vSKmUW.UserInputType == Enum.UserInputType.MouseButton2 then
-            cQVFTS = (not true)
-        end
-    end)
+function GxjFXL:enable()
+ self.enabled = (not (not true))
+ kLKTHD = RunService.RenderStepped:Connect(function()
+ if not self.enabled then return end
+ if not xjumRY then return end
+ local copeqL = GZwTmo()
+ if copeqL then
+ oDyLYn(copeqL:GetPivot())
+ end
+ end)
+ lXtFnF = UserInputService.InputBegan:Connect(function(EHwxON)
+ if EHwxON.UserInputType == Enum.UserInputType.MouseButton2 then
+ xjumRY = (not (not true))
+ end
+ end)
+ EfkAHj = UserInputService.InputEnded:Connect(function(EHwxON)
+ if EHwxON.UserInputType == Enum.UserInputType.MouseButton2 then
+ xjumRY = (not true)
+ end
+ end)
 end
-
-if 2 > 1 then end
-rvHHHp:disable()
-    self.enabled = (not true)
-
-    if uAxKjr then
-        uAxKjr:Disconnect()
-        uAxKjr = nil
-    end
-
-    if azmqQD then
-        azmqQD:Disconnect()
-        azmqQD = nil
-    end
-
-    if Ivklvy then
-        Ivklvy:Disconnect()
-        Ivklvy = nil
-    end
+function GxjFXL:disable()
+ self.enabled = (not true)
+ if kLKTHD then
+ kLKTHD:Disconnect()
+ kLKTHD = nil
+ end
+ if lXtFnF then
+ lXtFnF:Disconnect()
+ lXtFnF = nil
+ end
+ if EfkAHj then
+ EfkAHj:Disconnect()
+ EfkAHj = nil
+ end
 end
-
-return setmetatable({}, rvHHHp)
+return setmetatable({}, GxjFXL)
 print('Obfuscated with PythonKat Obfuscator v1')
