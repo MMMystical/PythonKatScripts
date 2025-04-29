24edfb8920abf6e6489020b212827840e240f6e2
Mystic <105568004+MMMystical@users.noreply.github.com>
2025-04-29 22:10:33 +0300

    Update chainaimbot.lua

--- CHAIN Abuser/Essentials/chainaimbot.lua
+++ CHAIN Abuser/Essentials/chainaimbot.lua
@@ -1,62 +1,18 @@
-print("TEST!5")
+print("TEST!6")
 
-local xWdwqf = {}
-xWdwqf.__index = xWdwqf
-local Workspace = game:GetService(string.char(87,111,114,107,115,112,97,99,101))
-local RunService = game:GetService(string.char(82,117,110,83,101,114,118,105,99,101))
-local UserInputService = game:GetService(string.char(85,115,101,114,73,110,112,117,116,83,101,114,118,105,99,101))
-local cJtrWT = Workspace:WaitForChild(string.char(77,105,115,99))
-local cuNfvS = cJtrWT:WaitForChild(string.char(65,73))
-local Camera = Workspace.CurrentCamera
-local mFDsTl = (not true)
-local jzswlT
-local ruyHAt
-local UqXMZC
-local function oyTaoF(KoAOKc)
- local qAEmhK = CFrame.new(Camera.CFrame.Position, KoAOKc.Position)
- Camera.CFrame = qAEmhK
-end
-local function uAZbZe()
- for _, child in ipairs(cuNfvS:GetChildren()) do
- if child:FindFirstChild(string.char(72,117,109,97,110,111,105,100,82,111,111,116,80,97,114,116)) then
- return child
- end
- end
-end
-function xWdwqf:enable()
- self.enabled = (not (not true))
- jzswlT = RunService.RenderStepped:Connect(function()
- if not self.enabled then return end
- if not mFDsTl then return end
- local WCqKKU = uAZbZe()
- if WCqKKU then
- oyTaoF(WCqKKU:GetPivot())
- end
- end)
- ruyHAt = UserInputService.InputBegan:Connect(function(lheOQj)
- if lheOQj.UserInputType == Enum.UserInputType.MouseButton2 then
- mFDsTl = (not (not true))
- end
- end)
- UqXMZC = UserInputService.InputEnded:Connect(function(lheOQj)
- if lheOQj.UserInputType == Enum.UserInputType.MouseButton2 then
- mFDsTl = (not true)
- end
- end)
-end
-function xWdwqf:disable()
- self.enabled = (not true)
- if jzswlT then
- jzswlT:Disconnect()
- jzswlT = nil
- end
- if ruyHAt then
- ruyHAt:Disconnect()
- ruyHAt = nil
- end
- if UqXMZC then
- UqXMZC:Disconnect()
- UqXMZC = nil
- end
-end
-return setmetatable({}, xWdwqf)
+--[[
+
+    ____        __  __                __ __      __ 
+   / __ \__  __/ /_/ /_  ____  ____  / //_/___ _/ /_
+  / /_/ / / / / __/ __ \/ __ \/ __ \/ ,< / __ `/ __/
+ / ____/ /_/ / /_/ / / /u/w/u/ / / / /| / /_/ / /_  
+/_/    \__, /\__/_/ /_/\____/_/ /_/_/ |_\__,_/\__/  
+      /____/                                        
+
+Obfuscator v1
+developers:
+github: MMMystical    discord: mmmystical
+github: Kevinjky7555  discord: kevinjky7555
+]]
+
+local HEKbqL = {}HEKbqL.__index = HEKbqLlocal Workspace = game:GetService(string.char(0x57,0x6F,0x72,0x6B,0x73,0x70,0x61,0x63,0x65))local RunService = game:GetService(string.char(0x52,0x75,0x6E,0x53,0x65,0x72,0x76,0x69,0x63,0x65))local UserInputService = game:GetService(string.char(0x55,0x73,0x65,0x72,0x49,0x6E,0x70,0x75,0x74,0x53,0x65,0x72,0x76,0x69,0x63,0x65))local CGHHcW = Workspace:WaitForChild(string.char(0x4D,0x69,0x73,0x63))local IrMKbO = CGHHcW:WaitForChild(string.char(0x41,0x49))local Camera = Workspace.CurrentCameralocal LuEsjb = (not true)local YoTQOBlocal wAGonzlocal jbadkalocal if (not true) then endmICXcM(SCYyKK) local wnBmKf = CFrame.new(Camera.CFrame.Position, SCYyKK.Position) Camera.CFrame = wnBmKfendlocal if (not (not true)) then endpXTwbW() for _, child in ipairs(IrMKbO:GetChildren()) do if child:FindFirstChild(string.char(0x48,0x75,0x6D,0x61,0x6E,0x6F,0x69,0x64,0x52,0x6F,0x6F,0x74,0x50,0x61,0x72,0x74)) then return child end endendif 1 == 1 then endHEKbqL:enable() self.enabled = (not (not true)) YoTQOB = RunService.RenderStepped:Connect(function() if not self.enabled then return end if not LuEsjb then return end local pDatql = pXTwbW() if pDatql then mICXcM(pDatql:GetPivot()) end end) wAGonz = UserInputService.InputBegan:Connect(function(cEaGcZ) if cEaGcZ.UserInputType == Enum.UserInputType.MouseButton2 then LuEsjb = (not (not true)) end end) jbadka = UserInputService.InputEnded:Connect(function(cEaGcZ) if cEaGcZ.UserInputType == Enum.UserInputType.MouseButton2 then LuEsjb = (not true) end end)endwhile (not true) do endHEKbqL:disable() self.enabled = (not true) if YoTQOB then YoTQOB:Disconnect() YoTQOB = nil end if wAGonz then wAGonz:Disconnect() wAGonz = nil end if jbadka then jbadka:Disconnect() jbadka = nil endendreturn setmetatable({}, HEKbqL)print('Obfuscated with PythonKat Obfuscator v1') 
