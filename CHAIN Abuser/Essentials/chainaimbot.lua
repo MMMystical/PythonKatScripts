0ea43966b89b9108a1efbbaafd6e0050fc68486a
Mystic <105568004+MMMystical@users.noreply.github.com>
2025-04-29 17:35:06 +0300

    Update chainaimbot.lua

--- CHAIN Abuser/Essentials/chainaimbot.lua
+++ CHAIN Abuser/Essentials/chainaimbot.lua
@@ -1,4 +1,4 @@
-print("TEST!3")
+print("TEST!4")
 
 local Module = {}
 Module.__index = Module
@@ -6,68 +6,59 @@ Module.__index = Module
 local Workspace = game:GetService(string.char(87,111,114,107,115,112,97,99,101))
 local RunService = game:GetService(string.char(82,117,110,83,101,114,118,105,99,101))
 local UserInputService = game:GetService(string.char(85,115,101,114,73,110,112,117,116,83,101,114,118,105,99,101))
-local CKCAOU = Workspace:WaitForChild(string.char(77,105,115,99))
-local xzkNVB = CKCAOU:WaitForChild(string.char(65,73))
+local phPgVE = Workspace:WaitForChild(string.char(77,105,115,99))
+local cGPnAH = phPgVE:WaitForChild(string.char(65,73))
 local Camera = Workspace.CurrentCamera
-local ZfkswD = (not true)
-local jXUHOC
-local HJBUAX
-local srxAkf
-local function MLobcC(FkuBrU)
- local FBnugu = CFrame.new(Camera.CFrame.Position, FkuBrU.Position)
- Camera.CFrame = FBnugu
+local eRIgEy = (not true)
+local RaUcJK
+local rgPpQO
+local jHvivq
+local function LGVmCX(OjSpSR)
+ local zZtZBS = CFrame.new(Camera.CFrame.Position, OjSpSR.Position)
+ Camera.CFrame = zZtZBS
 end
-local function ofkgiz()
- for _, child in ipairs(xzkNVB:GetChildren()) do
+local function JtDBYb()
+ for _, child in ipairs(cGPnAH:GetChildren()) do
  if child:FindFirstChild(string.char(72,117,109,97,110,111,105,100,82,111,111,116,80,97,114,116)) then
  return child
  end
  end
 end
-
 function Module:enable()
-    self.enabled = true
-
-    renderConnection = RunService.RenderStepped:Connect(function()
-        if not self.enabled then return end
-        if not ZfkswD then return end
-
-        local chain = ofkgiz()
-        if chain then
-            MLobcC(chain:GetPivot())
-        end
-    end)
-
-    inputBeganConnection = UserInputService.InputBegan:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton2 then
-            ZfkswD = true
-        end
-    end)
-
-    inputEndedConnection = UserInputService.InputEnded:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton2 then
-            ZfkswD = false
-        end
-    end)
+ self.enabled = (not (not true))
+ RaUcJK = RunService.RenderStepped:Connect(function()
+ if not self.enabled then return end
+ if not eRIgEy then return end
+ local tAUciE = JtDBYb()
+ if tAUciE then
+ LGVmCX(tAUciE:GetPivot())
+ end
+ end)
+ rgPpQO = UserInputService.InputBegan:Connect(function(BogMru)
+ if BogMru.UserInputType == Enum.UserInputType.MouseButton2 then
+ eRIgEy = (not (not true))
+ end
+ end)
+ jHvivq = UserInputService.InputEnded:Connect(function(BogMru)
+ if BogMru.UserInputType == Enum.UserInputType.MouseButton2 then
+ eRIgEy = (not true)
+ end
+ end)
 end
-
 function Module:disable()
-    self.enabled = false
-
-    if renderConnection then
-        renderConnection:Disconnect()
-        renderConnection = nil
-    end
-
-    if inputBeganConnection then
-        inputBeganConnection:Disconnect()
-        inputBeganConnection = nil
-    end
-
-    if inputEndedConnection then
-        inputEndedConnection:Disconnect()
-        inputEndedConnection = nil
-    end
+ self.enabled = (not true)
+ if RaUcJK then
+ RaUcJK:Disconnect()
+ RaUcJK = nil
+ end
+ if rgPpQO then
+ rgPpQO:Disconnect()
+ rgPpQO = nil
+ end
+ if jHvivq then
+ jHvivq:Disconnect()
+ jHvivq = nil
+ end
 end
 
 return setmetatable({}, Module)
